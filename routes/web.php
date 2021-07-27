ed717814abb088dc6427652c0f637b38a7fcfeaa
shafikcmt <mdshafiqulislam822@gmail.com>
2021-07-27 18:27:25 +0600

    layout and  home page design with laravel livewire

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use APP\Http\Livewire\HomeComponent;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/',HomeComponent::class);
+Route::get('/',App\Http\Livewire\HomeComponent::class);

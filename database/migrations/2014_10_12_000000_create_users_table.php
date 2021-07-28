b921e859658c39d66fb35393bdb92a45755f0b6c
shafikcmt <mdshafiqulislam822@gmail.com>
2021-07-28 09:35:55 +0600

    user and admin dashboard create also logout system design

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('utype')->default('USR')->comment('ADM for admin and USR for user or Customer');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
-use APP\Http\Livewire\HomeComponent;
-use APP\Http\Livewire\ShopComponent;
-use APP\Http\Livewire\CartComponent;
-use APP\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +21,17 @@ Route::get('/',App\Http\Livewire\HomeComponent::class);
 Route::get('/shop',App\Http\Livewire\ShopComponent::class);
 Route::get('/cart',App\Http\Livewire\CartComponent::class);
 Route::get('/checkout',App\Http\Livewire\CheckoutComponent::class);
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+//user
+Route::middleware(['auth:sanctum','verified'])->group(function(){
+    Route::get('/user/dashboard',App\Http\Livewire\User\UserDashboardComponent::class)->name('user.dashboard');
+});
+//Admin
+Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',App\Http\Livewire\Admin\AdminDashboardComponent::class)->name('admin.dashboard');
+    
+});

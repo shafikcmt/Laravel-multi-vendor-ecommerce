d38f5908b9ab5ac2f99c58adccc14865e34af6cb
shafikcmt <mdshafiqulislam822@gmail.com>
2021-07-28 21:42:52 +0600

    product and category factory page some changes

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class ProductFactory extends Factory
 {
@@ -21,8 +22,20 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $product_name = $this->faker->unique()->words($nb=4,$asText=true);
+        $slug = Str::slug($product_name);
         return [
-            //
+            'name' => $product_name,
+            'slug' => $slug,
+            'sort_discription' => $this->faker->text(200),
+            'discription' => $this->faker->text(500),
+            'regular_price' => $this->faker->numberBetween(10,500),
+            'SKU' => 'DIGI'.$this->faker->unique()->numberBetween(100,500),
+            'stock_status' => 'instock',
+            'quantity' => $this->faker->numberBetween(100,200),
+            'image' => 'digital_'. $this->faker->unique()->numberBetween(1.22).'.jpg',
+            'category_id' => $this->faker->numberBetween(1,5)
+            
         ];
     }
 }

baa1b28c0b4b1008fdc6f0d3ac33ed52d49b27fa
shafikcmt <mdshafiqulislam822@gmail.com>
2021-07-30 08:53:04 +0600

    Delete Cart Item system single and all cart item product

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -18,6 +18,15 @@ class CartComponent extends Component
         $qty = $product->qty - 1;
         Cart::update($rowId,$qty);
     }
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+        session()->flash('success_message','Item has been removed');
+    }
+    public function destroyAll()
+    {
+        Cart::destroy();
+    }
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');

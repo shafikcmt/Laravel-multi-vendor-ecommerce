7e34626ce6b8433e1536c40a49d7be3091329e81
shafikcmt <mdshafiqulislam822@gmail.com>
2021-07-30 08:38:37 +0600

    Dynamic Update Cart Quantity increase and decrease system with laravel

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -70,7 +70,7 @@
 									</div>
 									<div class="product-info">
 										<a href="{{route('product.details',['slug'=>$product->slug])}}" class="product-name"><span>{{$product->name}}</span></a>
-										<div class="wrap-price"><span class="product-price">{{$product->regular_price}}</span></div>
+										<div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
 										<a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}},'{{$product->name}}',{{$product->regular_price}})">Add To Cart</a>
 									</div>
 								</div>

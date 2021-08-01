48bd1104b0b9181bc1e79e248decca15a650faf0
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-01 21:33:35 +0600

    Admin All Product Page showing with laravel

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,6 @@ Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
     Route::get('/admin/categories',App\Http\Livewire\Admin\AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add',App\Http\Livewire\Admin\AdminAddCategoryComponent::class)->name('admin.addcategories');
     Route::get('/admin/category/edit/{category_slug}',App\Http\Livewire\Admin\AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/products',App\Http\Livewire\Admin\AdminProductComponent::class)->name('admin.products');
     
 });

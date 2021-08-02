74096d3b0bbbbefe703450fde00c90c0dc151bc2
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-02 23:55:52 +0600

    Admin Add New Product system create with laravel dynamic

--- resources/views/livewire/admin/admin-add-product-component.blade.php
+++ resources/views/livewire/admin/admin-add-product-component.blade.php
@@ -14,53 +14,56 @@
                         </div>
                     </div>
                     <div class="panel-body">
-                        <form action="" class="form-horizontal" onetype="multipart/form-data">
+                        @if(Session::has('message'))
+                        <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                        @endif
+                        <form action="" class="form-horizontal" onetype="multipart/form-data" wire:submit.prevent="addProduct">
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Product Name</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="Product Name" class="form-control input-md">
+                                    <input type="text" placeholder="Product Name" class="form-control input-md" wire:model="name" wire:keyup="generateSlug">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Product Slug</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="Product Slug" class="form-control input-md">
+                                    <input type="text" placeholder="Product Slug" class="form-control input-md" wire:model="slug">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Short Discription</label>
                                 <div class="col-md-4">
-                                   <textarea class="form-control" placeholder="Short Discription" name="" id="" cols="30" rows="10"></textarea>
+                                   <textarea class="form-control" placeholder="Short Discription" name="" id="" cols="30" rows="10" wire:model="sort_discription"></textarea>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Discription</label>
                                 <div class="col-md-4">
-                                   <textarea class="form-control" placeholder="Discription" name="" id="" cols="30" rows="10"></textarea>
+                                   <textarea class="form-control" placeholder="Discription" name="" id="" cols="30" rows="10" wire:model="discription"></textarea>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Regular Price</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="Regular Price" class="form-control input-md">
+                                    <input type="text" placeholder="Regular Price" class="form-control input-md" wire:model="regular_price">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Sale Price</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="Sale Price" class="form-control input-md">
+                                    <input type="text" placeholder="Sale Price" class="form-control input-md" wire:model="sale_price">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">SKU</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="SKU" class="form-control input-md">
+                                    <input type="text" placeholder="SKU" class="form-control input-md" wire:model="SKU">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Stock</label>
                                 <div class="col-md-4">
-                                   <select class="form-control" name="" id="">
+                                   <select class="form-control" name="" id="" wire:model="stock_status">
                                        <option value="stock">Stock</option>
                                        <option value="outofstock">Out of Stock</option>
                                    </select>
@@ -69,7 +72,7 @@
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Featured</label>
                                 <div class="col-md-4">
-                                   <select class="form-control" name="" id="">
+                                   <select class="form-control" name="" id="" wire:model="featured">
                                        <option value="0">No</option>
                                        <option value="1">Yes</option>
                                    </select>
@@ -78,19 +81,22 @@
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Quantity</label>
                                 <div class="col-md-4">
-                                    <input type="text" placeholder="Quantity" class="form-control input-md">
+                                    <input type="text" placeholder="Quantity" class="form-control input-md" wire:model="quantity">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Product Image</label>
                                 <div class="col-md-4">
-                                    <input type="file" class="input-file">
+                                    <input type="file" class="input-file" wire:model="image">
+                                    @if($image)
+                                    <img src="{{$image->temporaryUrl()}}" alt="" width="120">
+                                    @endif
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-md-4 control-label">Category</label>
                                 <div class="col-md-4">
-                                   <select class="form-control" name="" id="">
+                                   <select class="form-control" name="" id="" wire:model="category_id">
                                        <option value="">Select Category</option>
                                        @foreach($categories as $category)
                                        <option value="{{$category->id}}">{{$category->name}}</option>

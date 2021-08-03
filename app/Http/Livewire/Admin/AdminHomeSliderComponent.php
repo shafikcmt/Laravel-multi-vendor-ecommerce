d7433fc2c58cb011e5c738d3749e8f15924de7a8
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-04 05:58:07 +0600

    Admin Making Home Page Slider Dynamic delete system

--- app/Http/Livewire/Admin/AdminHomeSliderComponent.php
+++ app/Http/Livewire/Admin/AdminHomeSliderComponent.php
@@ -7,6 +7,12 @@ use App\Models\HomeSlider;
 
 class AdminHomeSliderComponent extends Component
 {
+    public function deleteSlide($slide_id)
+    {
+        $slider = HomeSlider::find($slide_id);
+        $slider->delete();
+        session()->flash('message','Slider has been deleted successfully!');
+    }
     public function render()
     {
         $sliders = HomeSlider::all();
--- resources/views/livewire/admin/admin-home-slider-component.blade.php
+++ resources/views/livewire/admin/admin-home-slider-component.blade.php
@@ -14,7 +14,10 @@
                         </div>    
                    </div>
                    <div class="panel-body">
-                        <table class="table table-striped">
+                       @if(Session::has('message'))
+                       <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                       @endif 
+                       <table class="table table-striped">
                             <thead>
                                 <tr>
                                     <th>Id</th>
@@ -42,6 +45,7 @@
                                 <td>{{$slider->created_at}}</td>
                                 <td>
                                     <a href="{{route('admin.edithomeslider',['slide_id'=>$slider->id])}}" ><i class="fa fa-edit fa-2x text-info"></i></a>
+                                    <a href="#" wire:click.prevent="deleteSlide({{$slider->id}})"><i class="fa fa-times fa-2x text-danger"></i></a>
                                 </td>
                             </tr>
                             @endforeach

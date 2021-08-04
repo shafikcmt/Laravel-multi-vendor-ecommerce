0e960fb3d635f5069c33b51fc680e893985da201
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-04 08:36:10 +0600

    table structure design and migrate for homecategory

--- database/migrations/2021_08_04_022929_create_home_categories_table.php
+++ database/migrations/2021_08_04_022929_create_home_categories_table.php
@@ -15,6 +15,8 @@ class CreateHomeCategoriesTable extends Migration
     {
         Schema::create('home_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('sel_categories');
+            $table->integer('no_of_products');
             $table->timestamps();
         });
     }
